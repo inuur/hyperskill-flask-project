d5b0483d04307652936c3949c8419d079a3662cb
inuur <inuur@mail.ru>
2020-07-11 12:25:29 +0300

    driver module review

--- Flask HyperSkill Project/stage1/tests.py
+++ Flask HyperSkill Project/stage1/tests.py
@@ -1,57 +1,19 @@
+from hstest.check_result import CheckResult
 from hstest.stage_test import StageTest
 from hstest.test_case import TestCase
-from hstest.check_result import CheckResult
-from selenium import webdriver
 
-import os
-import platform
+from driver import get_driver
 
 
-class DriverDetector:
-    WINDOWS = 'Windows'
-    LINUX = 'Linux'
-    MAC = 'Darwin'  # ??
-
-    @staticmethod
-    def get_driver():
-        system = platform.system()
-        if system == DriverDetector.WINDOWS:
-            return DriverDetector.get_windows_driver()
-        if system == DriverDetector.LINUX:
-            return DriverDetector.get_linux_driver()
-        if system == DriverDetector.MAC:
-            return DriverDetector.get_mac_driver()
-        raise Exception("Can't detect OS name")
-
-    @staticmethod
-    def get_windows_driver():
-        pass
-
-    @staticmethod
-    def get_mac_driver():
-        pass
-
-    @staticmethod
-    def get_linux_driver():
-        pass
-
-
-class TicTacToeTest(StageTest):
+class FlaskProjectTest(StageTest):
     def generate(self):
         return [TestCase()]
 
     def check(self, reply, attach):
-        system = platform.system()
-        if system == 'Windows':
-            print('this is windows!')
-        return CheckResult.correct()
-
-    @staticmethod
-    def get_driver():
-        system = platform.system()
-        if system == 'Windows':
-            print('this is windows!')
+        driver = get_driver()
+        print(driver)
+        return CheckResult.wrong()
 
 
 if __name__ == '__main__':
-    TicTacToeTest('web.main').run_tests()
+    FlaskProjectTest('web.main').run_tests()
