eeaa7e4a790f5ce2661cdab56d437c260dd16bbd
Ainur <inuur@mail.ru>
2020-07-22 09:26:22 +0300

    css grid

--- Flask HyperSkill Project/stage2/web/app.py
+++ Flask HyperSkill Project/stage2/web/app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('main.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='192.168.0.107')
+    app.run(debug=True, host='192.168.0.102')
